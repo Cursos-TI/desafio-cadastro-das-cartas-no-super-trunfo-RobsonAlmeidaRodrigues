47cb9d4f41f49c01e24ac8190612f5bbe8579e1b
RobsonAlmeidaRodrigues <robriffzero@gmail.com>
2025-02-08 18:16:46 +0000

    Eu cruei novos printf e scanf.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,28 @@
 //Teste larissa
 
 int main() {
+    char estado, nome[50];
+    int cidade;
+
+    printf ("Jogo Super Trunfo - Países\n");
+
+    printf("Casdastro das cartas\n");
+
+    printf("Nome do País: ");
+    scanf("%s", &nome);
+    
+    printf("Estado representado por uma letra.\n");
+    printf("Estado:");
+    scanf("%s", &nome);
+
+    printf("Cidade representado por um número.\n");
+    printf("Cidade:");
+    scanf("%d", &cidade);
+
+   
+
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -18,5 +40,5 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
+   
 }
